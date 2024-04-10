37f19ccc961fe4de0e20c26229ba55ec06343e22
Petru Valicov <petru.valicov@umontpellier.fr>
2024-04-10 15:31:46 +0200

    pas de GridPane dans le controleur

--- src/main/java/fr/umontpellier/iut/exercice8/LoginControl.java
+++ src/main/java/fr/umontpellier/iut/exercice8/LoginControl.java
@@ -2,7 +2,7 @@ package fr.umontpellier.iut.exercice8;
 
 import javafx.scene.layout.GridPane;
 
-public class LoginControl extends GridPane {
+public class LoginControl{
 
     private void okClicked() {
     }
