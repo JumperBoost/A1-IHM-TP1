ea6693495d2f66c5b8d302e5a606f57db61d9198
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-05-02 15:37:53 +0200

    Implémentation de l'exercice 9

--- src/main/java/fr/umontpellier/iut/exercice9/Animation.java
+++ src/main/java/fr/umontpellier/iut/exercice9/Animation.java
@@ -1,5 +1,6 @@
 package fr.umontpellier.iut.exercice9;
 
+import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -7,25 +8,56 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.Random;
+
 public class Animation extends Application {
+
+    private Duration getRandomDuration() {
+        Random random = new Random();
+        int ms = random.nextInt(10, 250);
+        return Duration.millis(ms);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         BorderPane root = new BorderPane();
         CustomButton customButton = new CustomButton();
         root.setCenter(customButton);
+        root.setStyle("-fx-background-color: black");
         Scene scene = new Scene(root, 400, 400);
 
-        Duration duration = Duration.millis(1500);
-        TranslateTransition transition1 = new TranslateTransition(duration, customButton);
+        TranslateTransition transition1 = new TranslateTransition(getRandomDuration(), customButton);
         transition1.setByX(150);
         transition1.setByY(-150);
-        transition1.setAutoReverse(true);
-        transition1.setCycleCount(2);
 
-//        SequentialTransition st = new SequentialTransition(transition1, transition2, transition3, transition4, transition5);
+        TranslateTransition transition2 = new TranslateTransition(getRandomDuration(), customButton);
+        transition2.setByX(0);
+        transition2.setByY(300);
 
-        customButton.setOnMousePressed(mouseEvent -> transition1.play());
+        TranslateTransition transition3 = new TranslateTransition(getRandomDuration(), customButton);
+        transition3.setByX(-300);
+        transition3.setByY(0);
+
+        TranslateTransition transition4 = new TranslateTransition(getRandomDuration(), customButton);
+        transition4.setByX(0);
+        transition4.setByY(-300);
+
+        TranslateTransition transition5 = new TranslateTransition(getRandomDuration(), customButton);
+        transition5.setByX(300);
+        transition5.setByY(0);
+
+        SequentialTransition st = new SequentialTransition(transition1, transition2, transition3, transition4, transition5);
+        st.setAutoReverse(true);
+        st.setCycleCount(2);
+        st.setOnFinished(actionEvent -> {
+            transition1.setDuration(getRandomDuration());
+            transition2.setDuration(getRandomDuration());
+            transition3.setDuration(getRandomDuration());
+            transition4.setDuration(getRandomDuration());
+            transition5.setDuration(getRandomDuration());
+        });
+
+        customButton.setOnMousePressed(mouseEvent -> st.play());
 
         primaryStage.setTitle("Animation");
         primaryStage.setScene(scene);
