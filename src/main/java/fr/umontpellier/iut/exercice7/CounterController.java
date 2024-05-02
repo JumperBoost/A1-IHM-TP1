ca1f370a7a178bfef9479e7f9575428b0c0bd4d6
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-05-02 14:40:01 +0200

    Implémentation de l'exercice 7

--- src/main/java/fr/umontpellier/iut/exercice7/CounterController.java
+++ src/main/java/fr/umontpellier/iut/exercice7/CounterController.java
@@ -1,22 +1,45 @@
 package fr.umontpellier.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
+    @FXML
+    private Label counterLabel;
+
+    @FXML
+    private Button decrementButton;
+
+    @FXML
+    private Button incrementButton;
+
+    @FXML
+    private Button resetButton;
 
     int counter = 0;
 
     public void increment() {
+        counterLabel.setText(String.valueOf(++counter));
     }
 
     public void decrement() {
+        counterLabel.setText(String.valueOf(--counter));
+    }
+
+    public void reset() {
+        counter = 0;
+        counterLabel.setText(String.valueOf(counter));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing CounterController...");
+        counterLabel.setText(String.valueOf(0));
    }
 }
